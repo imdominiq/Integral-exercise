c81abf4deaedf70be087a0617327f8560da2d0fa
Dominik <62477432+imdominiq@users.noreply.github.com>
2023-10-26 01:21:09 +0200

    Add files via upload

---
+++ Calkowanie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Calkowanie
+{
+    public class Calkowanie
+    {
+        public static void CalkowanieNieoznaczone(int najwiekszaPotega, int[] tablicaPrzedrostkow)
+        {
+            int index = 0;
+            for (int i = najwiekszaPotega; i > 0; i--)
+            {
+                string przedrostek = tablicaPrzedrostkow[index] + "/" + Convert.ToString(i + 1) + "*";
+                index++;
+                int potega = i + 1; ;
+                Console.Write(Convert.ToString(przedrostek) + "x^" + Convert.ToString(potega));
+                if (i > 1)
+                {
+                    Console.Write("+");
+                }
+            }
+            Console.Write(" + c");
+        }
+
+        public static void CalkowanieOznaczone(int najwiekszaPotega, int[] tablicaPrzedrostkow, int pierwszaWartosc, int drugaWartosc)
+        {
+            double pierwszyWynik = 0;
+            double drugiWynik = 0;
+            int index = 0;
+            for (int i = najwiekszaPotega; i > 0; i--)
+            {
+                pierwszyWynik = pierwszyWynik + tablicaPrzedrostkow[index] * Math.Pow(pierwszaWartosc, i);
+                drugiWynik = drugiWynik + tablicaPrzedrostkow[index] * Math.Pow(drugaWartosc, i);
+                index++;
+            }
+            Console.WriteLine("\nWynik = " + Convert.ToString(pierwszyWynik - drugiWynik));
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Calkowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool programZakonczony = false;
+            int najwiekszyWykladnik = 0;
+            bool najwiekszyWykladnikPrawidlowy = false;
+
+            while (programZakonczony == false)
+            {
+                Console.WriteLine("Podaj najwieksza potege dla x:");
+
+                while (najwiekszyWykladnikPrawidlowy == false)
+                {
+                    najwiekszyWykladnik = Convert.ToInt32(Console.ReadLine());
+                    if (najwiekszyWykladnik > 0)
+                    {
+                        najwiekszyWykladnikPrawidlowy = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wykladnik musi byc rowny lub wieszky od 0");
+                    }
+
+                }
+
+
+                int[] przedrostki = new int[najwiekszyWykladnik + 1];
+                int index = 0;
+                for (int i = najwiekszyWykladnik; i > 0; i--)
+                {
+                    Console.WriteLine("Podaj przedrostek dla x^" + Convert.ToString(i));
+                    int przedrostek = Convert.ToInt32(Console.ReadLine());
+                    przedrostki[index] = przedrostek;
+                    index++;
+                }
+                Console.WriteLine("\nFunkcja f(x): ");
+                for (int i = 0; i < przedrostki.Length - 1; i++)
+                {
+                    Console.Write(Convert.ToString(przedrostki[i]) + "x^" + Convert.ToString(najwiekszyWykladnik - i));
+                    if (najwiekszyWykladnik - i > 1)
+                    {
+                        Console.Write("+");
+                    }
+                }
+
+                bool isDone = false;
+
+                while (isDone == false)
+                {
+                    Console.WriteLine();
+
+                    Console.WriteLine("\n\nWybierz całkowanie:");
+                    Console.WriteLine("1. Oznaczone");
+                    Console.WriteLine("2. Nieoznaczone");
+                    Console.WriteLine("3. Zakonczenie Programu");
+
+                    int task = Convert.ToInt32(Console.ReadLine());
+
+                    if (task == 1)
+                    {
+                        Console.WriteLine("Wybrales calkowanie oznaczone");
+                        Console.WriteLine("\nPodaj pierwsza liczbe:");
+                        int pierwszaLiczba = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("\nPodaj druga liczbe:");
+                        int drugaLiczba = Convert.ToInt32(Console.ReadLine());
+
+                        Calkowanie.CalkowanieOznaczone(najwiekszyWykladnik, przedrostki, pierwszaLiczba, drugaLiczba);
+                    }
+                    if (task == 2)
+                    {
+                        Console.WriteLine("Wybrales calkowanie nieoznaczone");
+                        Calkowanie.CalkowanieNieoznaczone(najwiekszyWykladnik, przedrostki);
+                    }
+
+                    if (task == 3)
+                    {
+                        isDone = true;
+                        programZakonczony = true;
+                    }
+                }
+
+            }
+        }
+    }
+}
